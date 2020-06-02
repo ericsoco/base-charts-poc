4dd819916a88a380bb0405ffeb8f75aa9ef2402d
Eric Socolofsky <ericsoco@uber.com>
2020-06-02 10:59:24 -0700

    Fix bundle

--- packages/base-charts/webpack.config.js
+++ packages/base-charts/webpack.config.js
@@ -7,6 +7,26 @@ module.exports = {
   output: {
     path: resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
+    library: 'base-charts',
+    libraryTarget: 'umd',
+    // Webpack adds a `window` reference in `target:web` bundles,
+    // which precludes their use in universal apps (bundle cannot be
+    // imported in a node env). Until a `universal` target is available,
+    // this workaround conditionally provides `window`.
+    // Note this appears to cause mismatch between server- and client-rendered
+    // markup, so universal consumer apps will warn on client hydration.
+    // https://github.com/webpack/webpack/issues/6525#issuecomment-552140798
+    globalObject: `(() => {
+        if (typeof self !== 'undefined') {
+            return self;
+        } else if (typeof window !== 'undefined') {
+            return window;
+        } else if (typeof global !== 'undefined') {
+            return global;
+        } else {
+            return Function('return this')();
+        }
+    })()`,
   },
   module: {
     rules: [
@@ -22,6 +42,13 @@ module.exports = {
         },
       },
     ],
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+    },
   },
   // TODO: Move nivo to peerDependencies and externalize it
   // to reduce bundle size
